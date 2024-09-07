f269c5611894e0664e66709a57b383997d12bf54
Danny Torres <danny.torresxd@gmail.com>
2024-09-07 15:50:43 -0500

    Video 192

--- lib/presentation/widgets/movies_slideshow.dart
+++ lib/presentation/widgets/movies_slideshow.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
 import 'package:movies/domain/entities/movie.dart';
@@ -11,10 +12,17 @@ class MoviesSlideshow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
     return SizedBox(
       height: 210,
       width: double.infinity,
       child: Swiper(
+        pagination: SwiperPagination(
+          margin: const EdgeInsets.only(top: 0),
+          builder: DotSwiperPaginationBuilder(
+              activeColor: colors.primary, color: colors.secondary),
+        ),
         viewportFraction: 0.8,
         scale: 0.9,
         autoplay: true,
@@ -37,6 +45,36 @@ class _Slide extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    final decoration = BoxDecoration(
+      borderRadius: BorderRadius.circular(20),
+      boxShadow: const [
+        BoxShadow(
+          color: Colors.black45,
+          blurRadius: 10,
+          offset: Offset(0, 10),
+        ),
+      ],
+    );
+
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 30),
+      child: DecoratedBox(
+        decoration: decoration,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: Image.network(
+            movie.backdropPath,
+            fit: BoxFit.cover,
+            loadingBuilder: (context, child, loadingProgress) {
+              if (loadingProgress != null) {
+                return const DecoratedBox(
+                    decoration: BoxDecoration(color: Colors.black));
+              }
+              return FadeIn(child: child);
+            },
+          ),
+        ),
+      ),
+    );
   }
 }
