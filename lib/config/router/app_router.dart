d40c9c5ef00b1a79ffaae0bed5e00ef682258bac
Danny Torres <danny.torresxd@gmail.com>
2024-09-09 00:17:51 -0500

    video 210 child routes

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -6,19 +6,20 @@ final appRouter = GoRouter(
   initialLocation: HomeScreen.routePath,
   routes: [
     GoRoute(
-      path: HomeScreen.routePath,
-      name: HomeScreen.name,
-      builder: (context, state) => const HomeScreen(),
-    ),
-    GoRoute(
-      path: MovieScreen.routePath,
-      name: MovieScreen.routeName,
-      builder: (context, state) {
-        final movieId = state.pathParameters['id'] ?? 'no_id';
-        return MovieScreen(
-          movieId: movieId,
-        );
-      },
-    ),
+        path: HomeScreen.routePath,
+        name: HomeScreen.name,
+        builder: (context, state) => const HomeScreen(),
+        routes: [
+          GoRoute(
+            path: MovieScreen.routePath,
+            name: MovieScreen.routeName,
+            builder: (context, state) {
+              final movieId = state.pathParameters['id'] ?? 'no_id';
+              return MovieScreen(
+                movieId: movieId,
+              );
+            },
+          ),
+        ]),
   ],
 );
