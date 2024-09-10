45a93ced3195d83ac6357e960c74810f5ee5aa21
Danny Torres <danny.torresxd@gmail.com>
2024-09-10 01:13:02 -0500

    Video 213

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class MovieScreen extends StatelessWidget {
+class MovieScreen extends StatefulWidget {
   final String movieId;
   static const String routeName = 'movie_screen';
   static const String routePath = 'movie/:id';
@@ -10,10 +10,20 @@ class MovieScreen extends StatelessWidget {
   });
 
   @override
+  State<MovieScreen> createState() => _MovieScreenState();
+}
+
+class _MovieScreenState extends State<MovieScreen> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Movie Id: $movieId'),
+        title: Text('Movie Id: ${widget.movieId}'),
       ),
     );
   }
