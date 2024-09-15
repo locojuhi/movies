74e04ac4ae5d3c45d2cd05457b2e56c05a7bebd5
Danny Torres <danny.torresxd@gmail.com>
2024-09-15 00:14:04 -0500

    234

--- lib/presentation/delegates/search_movie_delegate.dart
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -42,7 +42,11 @@ class SearchMovieDelegate extends SearchDelegate<Movie?> {
           itemBuilder: (BuildContext context, int index) {
             final movie = movies[index];
 
-            return ListTile(title: _MovieItem(movie: movie));
+            return ListTile(
+                title: _MovieItem(
+              movie: movie,
+              onMovieSelected: close,
+            ));
           },
         );
       },
@@ -52,57 +56,63 @@ class SearchMovieDelegate extends SearchDelegate<Movie?> {
 
 class _MovieItem extends StatelessWidget {
   final Movie movie;
-  const _MovieItem({required this.movie});
+  final Function onMovieSelected;
+  const _MovieItem({required this.movie, required this.onMovieSelected});
 
   @override
   Widget build(BuildContext context) {
     final textStyle = Theme.of(context).textTheme;
     final size = MediaQuery.of(context).size;
 
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-      child: Row(
-        children: [
-          SizedBox(
-            width: size.width * 0.20,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(10),
-              child: Image.network(movie.posterPath),
-            ),
-          ),
-          SizedBox(
-            width: size.width * 0.6,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    movie.title,
-                    style: textStyle.titleMedium,
-                    overflow: TextOverflow.clip,
-                  ),
-                  (movie.overview.length > 100)
-                      ? Text('${movie.overview.substring(0, 100)}...')
-                      : Text(movie.overview),
-                  Row(
-                    children: [
-                      Icon(
-                        Icons.star_half_rounded,
-                        color: Colors.yellow.shade800,
-                      ),
-                      Text(
-                        HumanFormats.number(movie.voteAverage, 1).toString(),
-                        style: textStyle.bodyMedium!
-                            .copyWith(color: Colors.yellow.shade900),
-                      )
-                    ],
-                  ),
-                ],
+    return GestureDetector(
+      onTap: () {
+        onMovieSelected(context, movie);
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+        child: Row(
+          children: [
+            SizedBox(
+              width: size.width * 0.20,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image.network(movie.posterPath),
               ),
             ),
-          )
-        ],
+            SizedBox(
+              width: size.width * 0.6,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      movie.title,
+                      style: textStyle.titleMedium,
+                      overflow: TextOverflow.clip,
+                    ),
+                    (movie.overview.length > 100)
+                        ? Text('${movie.overview.substring(0, 100)}...')
+                        : Text(movie.overview),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.star_half_rounded,
+                          color: Colors.yellow.shade800,
+                        ),
+                        Text(
+                          HumanFormats.number(movie.voteAverage, 1).toString(),
+                          style: textStyle.bodyMedium!
+                              .copyWith(color: Colors.yellow.shade900),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/presentation/widgets/custom_appbar.dart
+++ lib/presentation/widgets/custom_appbar.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:movies/domain/entities/movie.dart';
 import 'package:movies/presentation/delegates/search_movie_delegate.dart';
 import 'package:movies/presentation/providers/movies/movies_repository_provider.dart';
 
@@ -34,13 +36,16 @@ class CustomAppbar extends ConsumerWidget {
               const Spacer(),
               IconButton(
                 onPressed: () {
-                  showSearch(
+                  showSearch<Movie?>(
                     context: context,
                     delegate: SearchMovieDelegate(
                       searchMovies:
                           ref.read(movieRepositoryProvider).searchMovies,
                     ),
-                  );
+                  ).then((movie) {
+                    if (movie == null) return;
+                    context.push('/movie/${movie.id}');
+                  });
                 },
                 icon: const Icon(Icons.search),
               )
