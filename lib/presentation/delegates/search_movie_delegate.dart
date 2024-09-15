0aa0f01764da0339c08b6b457cb5e5b99aed53e7
Danny Torres <danny.torresxd@gmail.com>
2024-09-14 22:50:23 -0500

    230

--- lib/presentation/delegates/search_movie_delegate.dart
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:movies/domain/entities/movie.dart';
 
-class SearchMovieDelegate extends SearchDelegate {
+class SearchMovieDelegate extends SearchDelegate<Movie?> {
   @override
   List<Widget>? buildActions(BuildContext context) {
     return [
-      const Text('buildActions'),
+      if (query.isNotEmpty)
+        IconButton(onPressed: () => query = '', icon: const Icon(Icons.clear))
     ];
   }
 
   @override
   Widget? buildLeading(BuildContext context) {
-    return const Text('buildLeading');
+    return IconButton(
+      onPressed: () => close(context, null),
+      icon: const Icon(Icons.arrow_back),
+    );
   }
 
   @override
